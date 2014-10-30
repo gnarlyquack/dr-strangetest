ba24ef2cfa9f8febd8bfe1974ca7fd0cb1c033c5
Karl Nack <karlnack@fastmail.us>
2014-10-30 00:22:07 -0500

    Run setup() before and teardown() after the test method

--- easytest.php
+++ easytest.php
@@ -4,18 +4,36 @@ namespace easytest;
 
 final class Runner {
     public function run_test_method($test, $method) {
+        if (is_callable([$test, 'setup'])) {
+            $test->setup();
+        }
         $test->$method();
+        if (is_callable([$test, 'teardown'])) {
+            $test->teardown();
+        }
     }
 }
 
 
 class TestRunner {
+    private $runner;
+
+    public function setup() {
+        $this->runner = new Runner();
+    }
+
     public function test_test_method() {
-        $tester = new Runner();
         $test = new SimpleTestCase();
         assert('[] === $test->log');
-        $tester->run_test_method($test, 'test');
+        $this->runner->run_test_method($test, 'test');
         assert('["test"] === $test->log');
+    }
+
+    public function test_setup_and_teardown() {
+        $test = new FixtureTestCase();
+        assert('[] === $test->log');
+        $this->runner->run_test_method($test, 'test');
+        assert('["setup", "test", "teardown"] === $test->log');
     }
 }
 
@@ -27,6 +45,17 @@ class SimpleTestCase {
     }
 }
 
+class FixtureTestCase extends SimpleTestCase {
+    public function setup() {
+        $this->log[] = __FUNCTION__;
+    }
+
+    public function teardown() {
+        $this->log[] = __FUNCTION__;
+    }
+}
+
 
 $runner = new Runner();
 $runner->run_test_method(new TestRunner(), 'test_test_method');
+$runner->run_test_method(new TestRunner(), 'test_setup_and_teardown');
