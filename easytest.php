b9e0aa544754fb4747a7e3e4fe7d5b7427c71c09
Karl Nack <karlnack@fastmail.us>
2014-11-01 00:49:31 -0500

    Report an exception's source and reason
    TestRunner now provides a stub implemention of the Reporter to simplify
    checking test results, which means the Reporter is now tested by a
    separate test case. A Reporter interface is used to ensure the stub
    interface matches the actual one.

--- easytest.php
+++ easytest.php
@@ -2,6 +2,12 @@
 
 namespace easytest;
 
+interface IReporter {
+    public function report_success();
+
+    public function report_error($source, $message);
+}
+
 
 final class Runner {
     private $reporter;
@@ -26,7 +32,10 @@ final class Runner {
             $this->reporter->report_success();
         }
         catch (\Exception $e) {
-            $this->reporter->report_error();
+            $this->reporter->report_error(
+                sprintf('%s::%s', get_class($test), $method),
+                $e
+            );
         }
 
         if (is_callable([$test, 'teardown'])) {
@@ -36,18 +45,18 @@ final class Runner {
 }
 
 
-final class Reporter {
+final class Reporter implements IReporter {
     private $report = [
         'Tests' => 0,
-        'Errors' => 0,
+        'Errors' => [],
     ];
 
     public function report_success() {
         ++$this->report['Tests'];
     }
 
-    public function report_error() {
-        ++$this->report['Errors'];
+    public function report_error($source, $message) {
+        $this->report['Errors'][] = [$source, $message];
     }
 
     public function get_report() {
@@ -56,32 +65,63 @@ final class Reporter {
 }
 
 
-class TestRunner {
-    private $reporter;
+abstract class TestCase {
+    protected final function assert_identical($expected, $actual) {
+        if ($expected !== $actual) {
+            throw new \Exception(
+                sprintf(
+                    "\n\nexpected: %s\n\nactual: %s\n\n",
+                    var_export($expected, true),
+                    var_export($actual, true)
+                )
+            );
+        }
+    }
+}
+
+class TestRunner extends TestCase implements IReporter {
     private $runner;
+    private $report;
 
     public function setup() {
-        $this->reporter = new Reporter();
-        $this->runner = new Runner($this->reporter);
+        $this->runner = new Runner($this);
+        $this->report = [
+            'Tests' => 0,
+            'Errors' => [],
+        ];
+    }
+
+    // implementation of reporter interface
+
+    public function report_success() {
+        ++$this->report['Tests'];
+    }
+
+    public function report_error($source, $message) {
+        $this->report['Errors'][] = [$source, $message->getMessage()];
     }
 
     // helper assertions
 
     private function assert_run($test, $expected) {
-        assert('[] === $test->log');
+        $this->assert_identical([], $test->log);
         $this->runner->run_test_case($test);
-        assert('$expected === $test->log');
+        $this->assert_identical($expected, $test->log);
     }
 
     private function assert_report($expected) {
-        assert('$expected === $this->reporter->get_report()');
+        $expected = array_merge(
+            ['Tests' => 0, 'Errors' => []],
+            $expected
+        );
+        $this->assert_identical($expected, $this->report);
     }
 
     // tests
 
     public function test_run_test_method() {
         $this->assert_run(new SimpleTestCase(), ['test']);
-        $this->assert_report(['Tests' => 1, 'Errors' => 0]);
+        $this->assert_report(['Tests' => 1]);
     }
 
     public function test_setup_and_teardown() {
@@ -92,7 +132,7 @@ class TestRunner {
                 'setup', 'test2', 'teardown',
             ]
         );
-        $this->assert_report(['Tests' => 2, 'Errors' => 0]);
+        $this->assert_report(['Tests' => 2]);
     }
 
     public function test_exception() {
@@ -100,7 +140,12 @@ class TestRunner {
             new ExceptionTestCase(),
             ['setup', 'test', 'teardown']
         );
-        $this->assert_report(['Tests' => 0, 'Errors' => 1]);
+        $this->assert_report([
+            'Tests' => 0,
+            'Errors' => [
+                ['easytest\\ExceptionTestCase::test', 'How exceptional!'],
+            ],
+        ]);
     }
 }
 
@@ -145,7 +190,42 @@ class ExceptionTestCase {
 
     public function test() {
         $this->log[] = __FUNCTION__;
-        throw new \Exception();
+        throw new \Exception('How exceptional!');
+    }
+}
+
+
+class TestReporter extends TestCase {
+    private $reporter;
+
+    public function setup() {
+        $this->reporter = new Reporter();
+    }
+
+    // helper assertions
+
+    private function assert_report($expected) {
+        $expected = array_merge(
+            ['Tests' => 0, 'Errors' => []],
+            $expected
+        );
+        $this->assert_identical($expected, $this->reporter->get_report());
+    }
+
+    // tests
+
+    public function test_blank_report() {
+        $this->assert_report([]);
+    }
+
+    public function test_report_success() {
+        $this->reporter->report_success();
+        $this->assert_report(['Tests' => 1]);
+    }
+
+    public function test_report_error() {
+        $this->reporter->report_error('source', 'message');
+        $this->assert_report(['Errors' => [['source', 'message']]]);
     }
 }
 
@@ -153,11 +233,22 @@ class ExceptionTestCase {
 $reporter = new Reporter();
 $runner = new Runner($reporter);
 $runner->run_test_case(new TestRunner());
+$runner->run_test_case(new TestReporter());
 
 $totals = [];
-foreach ($reporter->get_report() as $type => $count) {
-    if ($count) {
-        $totals[] = "$type: $count";
+foreach ($reporter->get_report() as $type => $results) {
+    if (!$results) {
+        continue;
+    }
+    if (is_array($results)) {
+        $totals[] = sprintf('%s: %d', $type, count($results));
+        echo str_pad("     $type     ", 70, '*', STR_PAD_BOTH), "\n\n\n";
+        foreach ($results as $i => $result) {
+            printf("%d) %s\n%s\n\n\n", $i + 1, $result[0], $result[1]);
+        }
+    }
+    else {
+        $totals[] = "$type: $results";
     }
 }
 echo implode(', ', $totals), "\n";
